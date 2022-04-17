455a46e9db7cf9bbfa9025f4353eb2d3c3ff3e2e
kennice <kennice123wong@gmail.com>
2022-04-18 07:57:09 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
+
+const Cat = () => {
+  const getFullName = (First, Second, Third) => {
+    return First + " " + Second + " " + Third;
+  }
+
+  return(
+    <View style={styles.container}>
+      <Text>Hiiiiiii I am {getFullName("Maru", "The", "Cat")}!</Text>
+      {/* <TextInput style={styles.input} defaultValue="Name me!" /> */}
+    </View>
+  )
+}
+
+const Cafe = () => {
+  return(
+    <View style={styles.container}>
+      <Text>Welcome!</Text>
+      <Cat/>
+      <Cat/>
+      <Cat/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    // justifyContent:'center',
+    // alignItems:'center'
+    paddingTop: 20,
+  },
+
+  input:{
+    height: 30,
+    borderColor: "gray",
+    borderWidth: 1,
+  }
+})
+
+export default Cafe;
+
+
